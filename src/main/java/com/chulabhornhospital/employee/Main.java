c8c894dc1099a97e348ecd51f19005d027ca0f68
Chanwit Kaewkasi <chanwit@gmail.com>
2015-10-21 21:13:05 +0700

    Add a simple main class (task - run)

---
+++ src/main/java/com/chulabhornhospital/employee/Main.java
@@ -0,0 +1,12 @@
+package com.chulabhornhospital.employee;
+
+/**
+ * Created by Administrator on 10/21/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
