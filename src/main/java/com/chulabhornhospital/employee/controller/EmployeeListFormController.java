238a2b2c4ac0ff2cfca14e9d987ad02e2f1e4402
Chanwit Kaewkasi <chanwit@gmail.com>
2015-11-14 22:33:35 +0700

    implement `get` for the controller

--- src/main/java/com/chulabhornhospital/employee/controller/EmployeeListFormController.java
+++ src/main/java/com/chulabhornhospital/employee/controller/EmployeeListFormController.java
@@ -3,12 +3,14 @@ package com.chulabhornhospital.employee.controller;
 import com.chulabhornhospital.employee.domain.Department;
 import com.chulabhornhospital.employee.domain.Employee;
 import com.chulabhornhospital.employee.form.EmployeeList;
+import com.chulabhornhospital.employee.mapper.EmployeeMapper;
 import com.chulabhornhospital.employee.mapper.custom.DepartmentListMapper;
 import com.chulabhornhospital.employee.mapper.custom.EmployeeListMapper;
 
 import java.util.List;
 
 import static com.chulabhornhospital.employee.util.SessionUtils.with;
+import static com.chulabhornhospital.employee.util.SessionUtils.withReturn;
 
 public class EmployeeListFormController {
 
@@ -21,11 +23,17 @@ public class EmployeeListFormController {
     public void listEmployees() throws Throwable {
         with((session) -> {
             EmployeeListMapper em = session.getMapper(EmployeeListMapper.class);
-            final List<Employee> result = em.list(10L);
+            final List<Employee> result = em.list(50L);
             form.setEmployees(result);
         });
     }
 
+    public Employee get(Long id) throws Throwable {
+        return withReturn((session) -> {
+            EmployeeMapper em = session.getMapper(EmployeeMapper.class);
+            return em.selectByPrimaryKey(id);
+        });
+    }
 
     public void search(String text) throws Throwable {
         with((session) -> {
