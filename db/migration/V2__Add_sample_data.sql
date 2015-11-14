bd4f563c2c9d36990a0277fbb11c72a7f65e4800
Chanwit Kaewkasi <chanwit@gmail.com>
2015-11-14 13:30:00 +0700

    tweak `gender` back to be Boolean. Better for radioButton binding

--- db/migration/V2__Add_sample_data.sql
+++ db/migration/V2__Add_sample_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO PUBLIC.EMPLOYEE
   (ID, FIRST_NAME, LAST_NAME, GENDER, DOB, SALARY, NICK_NAME, BEING_HIRED) VALUES
-  (1, 'Geronimo', 'Alba', 'M', '1976-10-25', 25000.00, 'Gero', true);
+  (1, 'Geronimo', 'Alba', true, '1976-10-25', 25000.00, 'Gero', true);
 
 INSERT INTO PUBLIC.EMPLOYEE
   (ID, FIRST_NAME, LAST_NAME, GENDER, DOB, SALARY, NICK_NAME, BEING_HIRED) VALUES
-  (2, 'Banana', 'Split', 'M', '1976-11-20', 30000.00, 'Banana', true);
+  (2, 'Banana', 'Split', true, '1976-11-20', 30000.00, 'Banana', true);
